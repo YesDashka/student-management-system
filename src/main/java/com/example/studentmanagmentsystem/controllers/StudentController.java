a761e7490107200dbfc7f722a0c0b9a164fcaf08
dasha <dasha>
2023-07-26 16:09:27 +0300

    Add student controller

---
+++ src/main/java/com/example/studentmanagmentsystem/controllers/StudentController.java
@@ -0,0 +1,52 @@
+package com.example.studentmanagmentsystem.controllers;
+
+import com.example.studentmanagmentsystem.models.Student;
+import com.example.studentmanagmentsystem.service.StudentServiceImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+    private StudentServiceImpl studentService;
+
+    public StudentController(StudentServiceImpl studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/students")
+    public String students(Model model) {
+        model.addAttribute("students", studentService.getAllStudents());
+        return "students";
+    }
+
+    @GetMapping("/students/new")
+    public String addStudent(Model model) {
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "new_student";
+    }
+
+    @PostMapping("/students")
+    public String saveStudent(@ModelAttribute Student student) {
+        studentService.saveStudent(student);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students/delete/{id}")
+    public String deleteStudent(@PathVariable long id) {
+        studentService.deleteStudentById(id);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/update/{id}")
+    public String showUpdateForm(@PathVariable long id, Model model) {
+        Student student = studentService.getStudentById(id);
+        model.addAttribute("student", student);
+        return "update_student";
+
+    }
+}
